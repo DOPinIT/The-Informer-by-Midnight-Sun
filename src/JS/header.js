c2cd70089a1ed7274c074b0fa5c1d12373570561
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-03-01 16:42:24 +0200

    switch-pages
    Зробив перемикання між сторінками через JS
    Видалив пустий hero.html (здається він не потрібен)
    Почистив від зайвого сміття read.html і favorite.html

--- src/JS/header.js
+++ src/JS/header.js
@@ -1,20 +1,12 @@
 // * Перемикання на необхідну сторінку (підкреслювання під назвою меню)
 // Додаю клас.menu__link--current до необхідного посилання і знімаю його з інших посилань
+const current = document.querySelector('.menu__link--current');
+current.classList.remove('menu__link--current');
 
-const navMenu = document.querySelector('.nav');
-navMenu.addEventListener('click', makeActiveLink);
-
-function makeActiveLink(e) {
-  console.log('e', e);
-  console.log('e.target', e.target);
-  const current = document.querySelector('.menu__link--current');
-  console.log('makeActiveLink >>> current:', current);
-
-  if (current === e.target) {
-    return;
-  } else {
-    current.classList.remove('menu__link--current');
-    e.target.classList.add('menu__link--current');
+const links = document.querySelectorAll('.menu__link');
+for (let i = 0; i < links.length; i += 1) {
+  if (links[i].href === window.location.href) {
+    links[i].classList.add('menu__link--current');
   }
 }
 // */ Перемикання на необхідну сторінку (підкреслювання під назвою меню)
