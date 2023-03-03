2383f31468633beb5eb96de5f8028bb4ca808684
Blokhind <113214742+Blokhind@users.noreply.github.com>
2023-03-03 17:23:40 +0200

    try to fix

--- src/index.js
+++ src/index.js
@@ -19,5 +19,5 @@ import './JS/header';
 import './JS/switcherTheme'; //виправлена ппомилка в назві
 import './JS/mobileMenu';
 import './JS/addToFavorite';
-import './JS/modalTeam';
 import './JS/scrollToUp';
+import './JS/modalTeam';
