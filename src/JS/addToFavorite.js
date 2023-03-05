01388c982ab214f0d5be19cdcd9d4576d4ab041a
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-03-05 18:35:19 +0200

    add @includes to html

--- src/JS/addToFavorite.js
+++ src/JS/addToFavorite.js
@@ -1,5 +1,7 @@
 // * Доавання в обране - заготовка
 const placeToFavorite = document.querySelector('.plate--add-to-favorite') || '';
+console.log('placeToFavorite:', placeToFavorite);
+
 if (placeToFavorite) {
   placeToFavorite.addEventListener('click', toggleToFavorite);
 }
@@ -8,5 +10,12 @@ function toggleToFavorite() {
   const markFavorite = document.querySelector('.plate__icon--add-to-favorite');
   markFavorite.classList.toggle('is-favorite');
   // + додати запис заголовка в буфер сторінки
+  faforites.push();
 }
+
+// function addToFavorites(params) {}
+// const favorites = [];
+
+// localStorage.setItem('favorites', JSON.stringify(favorites));
+
 // */ Доавання в обране - заготовка
