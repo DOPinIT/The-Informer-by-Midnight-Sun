ec018d5df0192576485ef32fa30e5d61ba5c8ce0
Artem <113770041+Artemdream@users.noreply.github.com>
2023-03-06 17:15:31 +0200

    add style calendar & date

--- src/JS/calendar.js
+++ src/JS/calendar.js
@@ -1,32 +1,68 @@
-const daysTag = document.querySelector(".days"),
-  currentDate = document.querySelector(".current-date"),
-  prevNextIcon = document.querySelectorAll(".icons span");
+const daysList = document.querySelector('.days'),
+  currentDate = document.querySelector('.current-date'),
+  calendarBtn = document.querySelector('.select-list__btn--calendar'),
+  calendarBox = document.querySelector('.calendar_box'),
+  prevNextIcon = document.querySelectorAll('.icons svg');
+
+calendarBtn.addEventListener('click', openCalendar);
+
+function openCalendar(e) {
+  if (
+    e.target.classList.contains('select-list__btn--calendar') ||
+    e.target.classList.contains('calendar-prev') ||
+    e.target.classList.contains('calendar-next')
+  ) {
+    calendarBox.classList.toggle('calendar_isHidden');
+    closeCalendar();
+  }
+
+  return;
+}
+
+function closeCalendar() {
+  window.addEventListener('click', e => {
+    if (
+      e.target.classList.contains('select-list__btn--calendar') ||
+      e.target.classList.contains('select-list__icon') ||
+      e.target.nodeName === 'svg'
+    ) {
+      return;
+    }
+    calendarBox.classList.add('calendar_isHidden');
+  });
+}
 
 let date = new Date(),
   currYear = date.getFullYear(),
   currMonth = date.getMonth();
 
+// отримуємо сьогоднішню дату яка відображається в кнопці
+calendarBtn.textContent = date
+  .toLocaleString()
+  .split(',')[0]
+  .replace(/\./g, '/');
+
 const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
 ];
 
-const renderCalendar = () => {
-  let firstDayofMonth = new Date(currYear, currMonth, 0).getDay(), 
-    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), 
-    lastDayofMonth = new Date(currYear, currMonth + 1, -1).getDay(), 
-    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); 
-  let liTag = "";
+function renderCalendar() {
+  let firstDayofMonth = new Date(currYear, currMonth, 0).getDay(),
+    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),
+    lastDayofMonth = new Date(currYear, currMonth + 1, -1).getDay(),
+    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();
+  let liTag = '';
 
   for (let i = firstDayofMonth; i > 0; i--) {
     liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
@@ -37,41 +73,38 @@ const renderCalendar = () => {
       i === date.getDate() &&
       currMonth === new Date().getMonth() &&
       currYear === new Date().getFullYear()
-        ? "active"
-        : "";
+        ? 'active'
+        : '';
     liTag += `<li class="${isToday}">${i}</li>`;
   }
 
   for (let i = lastDayofMonth; i < 6; i++) {
-    
     liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
   }
-  currentDate.textContent = `${months[currMonth]} ${currYear}`; 
-  daysTag.innerHTML = liTag;
-};
-
+  currentDate.innerHTML = `${months[currMonth]} ${currYear}`;
+  daysList.innerHTML = liTag;
+  //   console.log(currentDate);
+}
 renderCalendar();
 
-prevNextIcon.forEach((icon) => {
-  
-  icon.addEventListener("click", () => {
-  currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
-
+prevNextIcon.forEach(icon => {
+  icon.addEventListener('click', () => {
+    currMonth = icon.id === 'prev' ? currMonth - 1 : currMonth + 1;
+    console.log(icon.id);
     if (currMonth < 0 || currMonth > 11) {
-   date = new Date(currYear, currMonth, new Date().getDate());
-      currYear = date.getFullYear(); 
-      currMonth = date.getMonth(); 
+      date = new Date(currYear, currMonth, new Date().getDate());
+      currYear = date.getFullYear();
+      currMonth = date.getMonth();
     } else {
-      date = new Date(); 
+      date = new Date();
     }
-    renderCalendar(); 
+    renderCalendar();
   });
 });
 
-const getWeekendDays = () => {
-  const days = document.querySelectorAll(".days li"); 
-
-  days.forEach((day) => {
+function getWeekendDays() {
+  const days = document.querySelectorAll('.days li');
+  days.forEach(day => {
     const dayOfWeek = new Date(
       currYear,
       currMonth,
@@ -79,13 +112,24 @@ const getWeekendDays = () => {
     ).getDay();
 
     if (dayOfWeek === 6 || dayOfWeek === 0) {
-      if (day.classList.contains("active")) {
-        return
+      if (day.classList.contains('active')) {
+        return;
       }
-      day.style.color = "#5F6775"
+      day.style.color = '#5F6775';
     }
   });
-};
+}
 
 getWeekendDays();
 
+// отримуємо дату з календаря по кліку для її відображення в кнопці
+
+daysList.addEventListener('click', e => {
+  const clickedDate = e.target.textContent;
+  const selectedDate = new Date(currYear, currMonth, clickedDate);
+  const dayString = selectedDate
+    .toLocaleString()
+    .split(',')[0]
+    .replace(/\./g, '/');
+  calendarBtn.textContent = dayString;
+});
