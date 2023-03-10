86ea0a7db8d53d1a9fa0f92cc761b9ae50fbb5b0
Dmytro-Kobets <kobets.dmytro9@gmail.com>
2023-03-10 13:29:53 +0200

    added features to search

--- src/JS/searchNews.js
+++ src/JS/searchNews.js
@@ -4,10 +4,15 @@ import {
   favoriteResponseMarkup,
   searchResponseMarkup,
 } from './markup';
+import Notiflix from 'notiflix';
+import { markupError } from './newsFilters.js';
+import Loading from './loading';
 
 const searchFormRef = document.getElementById('search-form');
 const searchInputRef = document.querySelector('.input-submit');
 const gallery = document.querySelector('.card');
+const pagination = document.querySelector('.tui-pagination');
+const loading = new Loading();
 
 searchFormRef.addEventListener('submit', onSubmit);
 
@@ -15,11 +20,21 @@ function onSubmit(e) {
   e.preventDefault();
   const formData = new FormData(e.target);
   const formProps = Object.fromEntries(formData);
-  console.log(formProps.searchQuery);
+  gallery.innerHTML = '';
+  loading.open(gallery);
+  pagination.classList.add('isHidden');
 
   new NewsApi()
     .articleSearchList(formProps.searchQuery)
     .then(response => {
+      loading.closed(gallery);
+      if (response.length == 0) {
+        gallery.innerHTML = markupError(
+          'We haven’t found news for this prompt'
+        );
+        return Notiflix.Notify.failure("There's no news for this prompt.");
+      }
+      pagination.classList.remove('isHidden');
       const pageMarkup = searchResponseMarkup(response);
       gallery.insertAdjacentHTML('beforeend', pageMarkup);
     })
